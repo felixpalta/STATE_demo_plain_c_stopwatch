11f695eb210c5756bd0863d14c65aa3a210ae98a
Felix Palta <fpalta90@gmail.com>
2015-03-02 02:02:09 +0300

    Add new actions for restarting and "restopping" the watch.

--- startedstate.c
+++ startedstate.c
@@ -10,8 +10,15 @@ static void stop_watch(WatchState *state)
     transition_to_stopped(state);
 }
 
+static void restart_watch(WatchState *state)
+{
+    printf("Restarting the Watch.\n");
+
+}
+
 void transition_to_started(WatchState *state)
 {
     watch_state_set_default_actions(state);
     state->stop_action = stop_watch;
+    state->start_action = restart_watch;
 }
--- stoppedstate.c
+++ stoppedstate.c
@@ -11,8 +11,14 @@ static void start_watch(WatchState *state)
     transition_to_started(state);
 }
 
+static void restop_watch(WatchState *state)
+{
+    printf("Watch already stopped.\n");
+}
+
 void transition_to_stopped(WatchState *state)
 {
     watch_state_set_default_actions(state);
     state->start_action = start_watch;
+    state->stop_action = restop_watch;
 }
